0abfc9404f4874d6831046d07d85471f49f0ed82
Anatoliy <g.a.s0508657252@gmail.com>
2021-03-29 01:22:03 +0300

    first commit

---
+++ terra/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+   profile    = "default"
+   region     = "eu-central-1"
+ }
+/*
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "hashicorp/aws"
+      version = ">= 1.0"
+    }
+  }
+}
+*/
